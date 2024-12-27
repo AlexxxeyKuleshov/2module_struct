5454529b291deea0d1a5a267e2dfc8a25a3cb1f3
Alexey Kuleshov <149209268+AlexxxeyKuleshov@users.noreply.github.com>
2024-12-27 11:27:57 +0300

    3sem

---
+++ 3sem/quicksort.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "quicksort.cpp"
+template <typename T>
+void quick_sort(T a[], int low, int high);
---
+++ 3sem/shellsort.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "shellsort.cpp"
+template <typename T>
+void shell_sort(T a[], int n);
